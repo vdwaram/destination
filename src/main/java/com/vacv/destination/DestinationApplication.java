2e0799246916775763ed0d37cf28a317a6181418
venkata dwarampudi <v_dwaram@hotmail.com>
2023-05-28 09:41:38 -0400

    feat: added Api contract definition using OpenApi 3.0.3

--- src/main/java/com/vacv/destination/DestinationApplication.java
+++ src/main/java/com/vacv/destination/DestinationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DestinationApplication {
 
-	 public static void main(String[] args) {
-		SpringApplication.run(DestinationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DestinationApplication.class, args);
+    }
 
 }
